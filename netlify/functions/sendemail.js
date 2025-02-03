151ecaaee699c2504df20883311f8c314f83bb43
zeesee123 <zubinchadha@gmail.com>
2025-02-03 15:27:21 +0530

    testing the email thing

--- netlify/functions/sendemail.js
+++ netlify/functions/sendemail.js
@@ -4,6 +4,19 @@ console.log(nodemailer);
 
 exports.handler=async function(event,context){
 
+
+    if (event.httpMethod !== 'POST') {
+        
+        return {
+            statusCode: 405, // Method Not Allowed
+            body: JSON.stringify({ message: 'Only POST method is allowed' })
+        };
+    }
+
+    // Parse the request body (it will be a JSON object)
+    const requestBody = JSON.parse(event.body);
+    const { name,comment,link } = requestBody;
+
     const transporter = nodemailer.createTransport({
         service: 'gmail',  // Using Gmail SMTP service
         auth: {
@@ -14,11 +27,11 @@ exports.handler=async function(event,context){
 
     // Email setup
     const mailOptions = {
-        from: process.env.GMAIL_USER,  // Sender email (your Gmail address)
+        from: 'zubinchadha@gmail.com',  // Sender email (your Gmail address)
         to: 'zubinchadha@gmail.com',  // Replace with your recipient's email address
-        subject: `New message from alan`,
-        text: `You have received a message from alan email`,
-        html: `<p>You have received a message from <strong>gibson</strong> teamwork</p>`
+        subject: `New message from ${name}`,
+        text: `You have received a message from alan email with comment: ${comment}`,
+        html: `<p>You have received a message from <strong>gibson</strong> teamwork</p><p>below is the link for the file</p><a href='${link}'>Link</a>`
     };
 
     // Send the email
